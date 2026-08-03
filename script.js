3786b76004a1c9180592562b3ebf830c3a5df609
ak81327-maker <ak81327@gmail.com>
2026-08-03 18:01:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.getElementById('orderForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('धन्यवाद! आपका ऑर्डर/संदेश प्राप्त हो गया है। नेक्शर टीम आपसे जल्द ही संपर्क करेगी।');
+    this.reset();
+});
 
