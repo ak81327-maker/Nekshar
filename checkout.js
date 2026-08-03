d7c1f7c62364a4b3961c7ceb45a713d19e257c49
ak81327-maker <ak81327@gmail.com>
2026-08-03 22:09:13 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,24 @@
+// ===========================
+// NEKSHAR AGARBATTI
+// CHECKOUT.JS
+// ===========================
 
+document.getElementById("checkoutForm").addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const mobile = document.getElementById("mobile").value;
+    const address = document.getElementById("address").value;
+
+    localStorage.setItem("customerName", name);
+    localStorage.setItem("customerMobile", mobile);
+    localStorage.setItem("customerAddress", address);
+
+    alert("Order Placed Successfully!");
+
+    localStorage.removeItem("cart");
+
+    window.location.href = "success.html";
+
+});
