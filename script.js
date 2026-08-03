97e6b0a438134504f9cf7a662fdc6a617eb66b95
ak81327-maker <ak81327@gmail.com>
2026-08-03 21:41:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-document.getElementById('orderForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('धन्यवाद! आपका ऑर्डर/संदेश प्राप्त हो गया है। नेक्शर टीम आपसे जल्द ही संपर्क करेगी।');
-    this.reset();
-});
+// ===============================
+// NEKSHAR AGARBATTI
+// SCRIPT.JS
+// ===============================
 
+// Cart
+let cart = [];
+
+// Add Product
+function addToCart(name, price) {
+    cart.push({
+        name: name,
+        price: price
+    });
+
+    alert(
+        name +
+        " has been added to your cart.\n\n" +
+        "Total Items: " +
+        cart.length
+    );
+}
+
+// Page Loaded
+window.onload = function () {
+    console.log("Nekshar Agarbatti Website Loaded Successfully");
+};
