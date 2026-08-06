fe3d4d84f8ab92e460df2a1e3b00bd860585d2e3
ak81327-maker <ak81327@gmail.com>
2026-08-06 11:27:59 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,42 @@
+document.getElementById("checkoutForm").addEventListener("submit", function(e){
 
+e.preventDefault();
+
+const name=document.getElementById("name").value;
+const mobile=document.getElementById("mobile").value;
+const address=document.getElementById("address").value;
+
+const payment=document.querySelector("input[name='payment']:checked").value;
+
+let utr="";
+
+if(payment==="UPI"){
+    utr=document.getElementById("utr").value.trim();
+
+    if(utr===""){
+        alert("Please enter UTR / Transaction ID");
+        return;
+    }
+}
+
+const message=
+`🛍️ *New Order - Nekshar Agarbatti*
+
+👤 Name: ${name}
+📱 Mobile: ${mobile}
+🏠 Address: ${address}
+
+💳 Payment: ${payment}
+
+${payment==="UPI" ? "🧾 UTR: "+utr : ""}`;
+
+window.open(
+`https://wa.me/91YOURNUMBER?text=${encodeURIComponent(message)}`,
+"_blank"
+);
+
+localStorage.removeItem("cart");
+
+window.location.href="success.html";
+
+});
